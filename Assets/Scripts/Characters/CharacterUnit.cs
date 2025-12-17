48074442691f6d7b88d9779bcfc11dbd29637bad
Deependra-Das <iamdeep75@gmail.com>
2025-12-17 12:48:36 +0530

    Added CharacterUnit Script

---
+++ Assets/Scripts/Characters/CharacterUnit.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CharacterUnit : MonoBehaviour
+{
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private int _baseDamage = 1;
+    [SerializeField] private int _baseHealth = 5;
+    [SerializeField] private int _baseShield = 0;
+    [SerializeField] private int _baseMana = 0;
+    [SerializeField] private int _baseHealing = 0;
+
+    [SerializeField] [Range(1, 5)] private int baseRange = 1;
+    [SerializeField] private float _baseAttackSpeed = 1f;
+    [SerializeField] private float _baseMovementSpeed = 1f;
+    [SerializeField] private float _baseManaRegenSpeed = 0f;
+
+    private float _damageMultiplier = 1f;
+    private float _healthMultiplier = 1f;
+    private float _shieldMultiplier = 1f;
+    private float _manaMultiplier = 1f;
+    private float _healingMultiplier = 1f;
+    private float _rangeMultiplier = 1f;
+    private float _attackSpeedMultiplier = 1f;
+    private float _manaRegenMultiplier = 1f;
+
+    protected float attackCoolDown;
+    protected CharacterUnit currentTarget = null;
+    protected Node currentNode;
+
+    protected bool isTargetInRange => currentTarget != null && Vector3.Distance(this.transform.position, currentTarget.transform.position) <= baseRange;
+    protected bool isMoving;
+    protected Node destination;
+
+    protected bool isDead = false;
+    protected bool canAttack = true;
+
+    public Node CurrentNode => currentNode;
+    protected bool HasEnemy => currentTarget != null;
+
+    void Start()
+    {
+        
+    }
+}
