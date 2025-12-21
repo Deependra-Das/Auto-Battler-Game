eed71d52d6bf38a2ba7e4ae145c3d9b479926cf6
Deependra-Das <iamdeep75@gmail.com>
2025-12-21 10:54:08 +0530

    Added UnitScriptableObject Script

---
+++ Assets/Scripts/CharactersUnits/ScriptableObjects/UnitScriptableObjectScript.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitScriptableObjectScript", menuName = "ScriptableObjects/UnitScriptableObjectScript")]
+public class UnitScriptableObjectScript : ScriptableObject
+{
+    public List<BaseUnit> unitPrefabList;
+}
