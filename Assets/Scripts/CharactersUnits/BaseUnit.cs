f651abb56f3fa6fd8183d3dd29522485bc66b417
Deependra-Das <iamdeep75@gmail.com>
2025-12-21 17:32:58 +0530

    Updated Base Unit Script to add Attack, Take Damage & Health Update

--- Assets/Scripts/CharactersUnits/BaseUnit.cs
+++ Assets/Scripts/CharactersUnits/BaseUnit.cs
@@ -1,34 +1,43 @@
 using AutoBattler.Main;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseUnit : MonoBehaviour
 {
-    [SerializeField] private string _characterName;
-    [SerializeField] private UnitTypeEnum _unitType;
-    [SerializeField] private SpriteRenderer _spriteRenderer;
-    [SerializeField] private Animator _animator;
+    [SerializeField] protected string characterName;
+    [SerializeField] protected UnitTypeEnum unitType;
+    [SerializeField] protected SpriteRenderer spriteRenderer;
+    [SerializeField] protected Animator animator;
 
-    [SerializeField] private int _baseDamage = 1;
-    [SerializeField] private int _baseHealth = 5;
-    [SerializeField] private int _baseShield = 0;
-    [SerializeField] private int _baseMana = 0;
-    [SerializeField] private int _baseHealing = 0;
+    [SerializeField] protected int baseDamage = 1;
+    [SerializeField] protected int baseHealth = 5;
+    [SerializeField] protected int baseShield = 0;
+    [SerializeField] protected int baseMana = 0;
+    [SerializeField] protected int baseHealing = 0;
 
-    [SerializeField] [Range(1, 5)] private int baseRange = 1;
-    [SerializeField] private float _baseAttackSpeed = 1f;
-    [SerializeField] private float _baseMovementSpeed = 1f;
-    [SerializeField] private float _baseManaRegenSpeed = 0f;
+    [SerializeField] [Range(1, 5)] protected int baseRange = 1;
+    [SerializeField] protected float baseAttackSpeed = 1f;
+    [SerializeField] protected float baseMovementSpeed = 1f;
+    [SerializeField] protected float baseManaRegenSpeed = 0f;
 
-    private float _damageMultiplier = 1f;
-    private float _healthMultiplier = 1f;
-    private float _shieldMultiplier = 1f;
-    private float _manaMultiplier = 1f;
-    private float _healingMultiplier = 1f;
-    private float _rangeMultiplier = 1f;
-    private float _attackSpeedMultiplier = 1f;
-    private float _manaRegenMultiplier = 1f;
+    [SerializeField] protected Slider healthBar;
+
+    protected int totalDamage;
+    protected int totalHealth;
+    protected int totalShield;
+    protected int totalMana;
+    protected int totalHealing;
+    protected int totalRange;
+    protected float totalAttackSpeed;
+    protected float totalMovementSpeed;
+    protected float totalManaRegenSpeed;
+
+    protected int currentHealth;
+    protected int currentShield;
+    protected int currentMana;
 
     protected float attackCoolDown;
     protected BaseUnit currentTarget = null;
@@ -41,10 +50,10 @@ public class BaseUnit : MonoBehaviour
     protected bool isDead = false;
     protected bool canAttack = true;
 
-    private TeamEnum _team;
+    protected TeamEnum _team;
 
-    public string CharacterName => _characterName;
-    public UnitTypeEnum UnitType => _unitType;
+    public string CharacterName => characterName;
+    public UnitTypeEnum UnitType => unitType;
     public TeamEnum Team => _team;
     public Node CurrentNode => currentNode;
     protected bool HasEnemy => currentTarget != null;
@@ -55,6 +64,10 @@ public class BaseUnit : MonoBehaviour
         this.currentNode = spawnNode;
         transform.position = currentNode.position;
         currentNode.SetOccupied(true);
+       totalHealth = baseHealth;
+       currentHealth = totalHealth;
+        healthBar.maxValue = totalHealth;
+        UpdateHealthBar(currentHealth);
     }
 
     protected void FindTarget()
@@ -83,13 +96,13 @@ public class BaseUnit : MonoBehaviour
         {
             GraphService graphService = GameManager.Instance.Get<GraphService>();
             destination = null;
-            List<Node> candidates = graphService.GetNodesCloseTo(currentTarget.CurrentNode);
-            candidates = candidates.OrderBy(x => Vector3.Distance(x.position, this.transform.position)).ToList();
-            for (int i = 0; i < candidates.Count; i++)
+            List<Node> availableNodes = graphService.GetNodesCloseTo(currentTarget.CurrentNode);
+            availableNodes = availableNodes.OrderBy(x => Vector3.Distance(x.position, this.transform.position)).ToList();
+            for (int i = 0; i < availableNodes.Count; i++)
             {
-                if (!candidates[i].IsOccupied)
+                if (!availableNodes[i].IsOccupied)
                 {
-                    destination = candidates[i];
+                    destination = availableNodes[i];
                     break;
                 }
             }
@@ -124,12 +137,47 @@ public class BaseUnit : MonoBehaviour
             return true;
         }
 
-        this.transform.position += direction.normalized * _baseMovementSpeed * Time.deltaTime;
+        this.transform.position += direction.normalized * baseMovementSpeed * Time.deltaTime;
         return false;
     }
 
     public void SetCurrentNode(Node node)
     {
         currentNode = node;
+    }
+
+    protected virtual void Attack()
+    {
+        if (!canAttack)
+            return;
+
+        attackCoolDown = 1 / baseAttackSpeed;
+        StartCoroutine(WaitCoroutine());
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        canAttack = false;
+        yield return null;
+        yield return new WaitForSeconds(attackCoolDown);
+        canAttack = true;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        UpdateHealthBar(currentHealth);
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            isDead = true;
+            currentNode.SetOccupied(false);
+            GameplayManager.Instance.MarkUnitDead(this);
+        }
+    }
+
+    public void UpdateHealthBar(float currentHealthValue)
+    {
+        healthBar.value = currentHealthValue;
     }
 }
