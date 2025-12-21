099f4ccc7c7d82f01889f2be28c8155f931f6a55
Deependra-Das <iamdeep75@gmail.com>
2025-12-21 10:55:01 +0530

    Updated GameManager to handle the scriptable Objects

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -1,24 +1,36 @@
 using AutoBattler.Utilities;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AutoBattler.Main
 {
     public class GameManager : GenericMonoSingleton<GameManager>
     {
+        [SerializeField] private TileScriptableObjectScript _tile_SO;
+        [SerializeField] private UnitScriptableObjectScript _unit_SO;
 
-        public const string UsernameKey = "Username";
-
-        private void Start()
+        private void Awake()
         {
             RegisterServices();
         }
 
+        private void Start()
+        {
+            GameplayManager.Instance.Initialize(_unit_SO);
+        }
+
         private void RegisterServices()
         {
+            ServiceLocator.Register(new TileGridService(_tile_SO));
+            ServiceLocator.Register(new GraphService());
+            ServiceLocator.Register(new TeamService());
         }
 
         private void DeregisterServices()
         {
+            ServiceLocator.Unregister<TileGridService>();
+            ServiceLocator.Unregister<GraphService>();
+            ServiceLocator.Unregister<TeamService>();
         }
 
         public T Get<T>()
