0cd5d5a0fc310aa6e3f59edc9c0088801b640248
Deependra-Das <iamdeep75@gmail.com>
2025-12-24 15:02:39 +0530

    Updated RangedAbilityService

--- Assets/Scripts/RangedAbilities/RangedAbilityService.cs
+++ Assets/Scripts/RangedAbilities/RangedAbilityService.cs
@@ -5,13 +5,15 @@ public class RangedAbilityService
     private Arrow _arrowPrefab;
     private ManaBurst _magicBurstPrefab;
 
+    private float arrowOffset = 0.35f;
+
     public RangedAbilityService(RangedAbilitiesScriptableObjectScript rangedAbilities_SO)
     {
         _arrowPrefab = rangedAbilities_SO.arrowPrefab;
         _magicBurstPrefab = rangedAbilities_SO.manaBurstPrefab;
     }
 
-    public void SpawnProjectile(RangedAbilityTypeEnum type, BaseUnit owner, BaseUnit target, int damage)
+    public void SpawnRangedAbility(RangedAbilityTypeEnum type, BaseUnit owner, BaseUnit target, int damage)
     {
         if (target == null) return;
 
@@ -29,8 +31,19 @@ public class RangedAbilityService
 
     private void SpawnArrow(BaseUnit owner, BaseUnit target, int damage)
     {
-        Arrow arrow = GameObject.Instantiate(_arrowPrefab, owner.transform.position, Quaternion.identity);
-        arrow.Initialize(target, damage);
+        Vector3 spawnPosition = owner.transform.position;
+        Vector3 adjustedTargetPosition = target.transform.position;
+
+        if (owner.DirectionFacing == UnitFacingDirectionEnum.Left || owner.DirectionFacing == UnitFacingDirectionEnum.Right)
+        {
+            spawnPosition += new Vector3(0, arrowOffset, 0);
+            adjustedTargetPosition += new Vector3(0, arrowOffset, 0);
+        }
+
+        Arrow arrow = GameObject.Instantiate(_arrowPrefab, spawnPosition, Quaternion.identity);
+
+        Vector3 adjustedDirection = (adjustedTargetPosition - spawnPosition);
+        arrow.Initialize(owner, target, damage, adjustedDirection.normalized, adjustedTargetPosition);
     }
 
     private void SpawnMagicCircle(BaseUnit target, int damage)
