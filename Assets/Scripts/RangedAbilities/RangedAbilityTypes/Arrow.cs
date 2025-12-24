55c119607ea8fe54664147cc7e4974e37508a3ae
Deependra-Das <iamdeep75@gmail.com>
2025-12-24 15:02:13 +0530

    Updated Arrow Script

--- Assets/Scripts/RangedAbilities/RangedAbilityTypes/Arrow.cs
+++ Assets/Scripts/RangedAbilities/RangedAbilityTypes/Arrow.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 
-public class Arrow : MonoBehaviour, IRangedAbility
+public class Arrow : MonoBehaviour
 {
+    private BaseUnit _ownerUnit;
     private BaseUnit _targetUnit;
     private int _damage;
     private float _speed = 7f;
     private Vector3 _direction;
     private float _hitDistance = 0.15f;
+    private Vector3 _adjustedTargetPosition;
 
-    public void Initialize(BaseUnit targetUnit, int dmg)
+    public void Initialize(BaseUnit ownerUnit, BaseUnit targetUnit, int dmg, Vector3 adjustedDirection, Vector3 adjustedTargetPosition)
     {
-        _targetUnit = targetUnit;
-        _damage = dmg;
-
-        if (_targetUnit != null)
+        if (targetUnit != null)
         {
-            _direction = (_targetUnit.transform.position - transform.position).normalized;
+            _ownerUnit = ownerUnit;
+            _targetUnit = targetUnit;
+            _damage = dmg;
+            _direction = adjustedDirection;
+            _adjustedTargetPosition = adjustedTargetPosition;
             RotateTowards(_direction);
         }
     }
@@ -30,16 +33,16 @@ public class Arrow : MonoBehaviour, IRangedAbility
 
         transform.position += _direction * _speed * Time.deltaTime;
 
-        if (Vector3.Distance(transform.position, _targetUnit.transform.position) <= _hitDistance)
+        if (Vector3.Distance(transform.position, _adjustedTargetPosition) <= _hitDistance)
         {
             _targetUnit.TakeDamage(_damage);
             Destroy(gameObject);
         }
     }
 
-    private void RotateTowards(Vector3 dir)
+    private void RotateTowards(Vector3 direction)
     {
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
