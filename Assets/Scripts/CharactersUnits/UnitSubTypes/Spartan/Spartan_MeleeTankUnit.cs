4a6a0aa49a4713dbc35c4317a4a56cf150c82c00
Deependra-Das <iamdeep75@gmail.com>
2026-01-07 20:24:57 +0530

    Added Spartan_MeleeTankUnit Script

---
+++ Assets/Scripts/CharactersUnits/UnitSubTypes/Spartan/Spartan_MeleeTankUnit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spartan_MeleeTankUnit : MeleeTankUnit
+{
+    protected override void Attack()
+    {
+        if (!canAttack) return;
+
+        Vector3 direction = (currentTarget.CurrentNode.position - this.transform.position);
+        Vector3 dirNormalized = direction.normalized;
+        animator.SetFloat("MoveX", dirNormalized.x);
+        animator.SetFloat("MoveY", dirNormalized.y);
+
+        PerformWarAxeAttack();
+    }
+
+    private void PerformWarAxeAttack()
+    {
+        animator.SetTrigger("Attack");
+        StartCoroutine(AttackCoolDownWaitCoroutine());
+    }
+
+    IEnumerator AttackCoolDownWaitCoroutine()
+    {
+        canAttack = false;
+        yield return null;
+        animator.ResetTrigger("Attack");
+        yield return new WaitForSeconds(attackCoolDown);
+        canAttack = true;
+    }
+}
