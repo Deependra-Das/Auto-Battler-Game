bcc601df2378a2eddc2c5f73e041f0780b5265a2
Deependra-Das <iamdeep75@gmail.com>
2026-01-10 20:12:47 +0530

    Added UnitData struct script

---
+++ Assets/Scripts/Units/UnitData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public struct UnitData
+{
+    public BaseUnit unitPrefab;
+    public string unitName;
+    public UnitTypeEnum unitType;
+    public string unitIcon;
+    public int unitCost;
+}
