76554c8f295c2ddec02fb025dc28f2cb3efe85fa
Deependra-Das <iamdeep75@gmail.com>
2026-01-12 18:11:44 +0530

    Updated Unit Data

--- Assets/Scripts/Units/UnitData.cs
+++ Assets/Scripts/Units/UnitData.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 [Serializable]
 public struct UnitData
 {
+    [HideInInspector] public int unitID;
     public BaseUnit unitPrefab;
     public Sprite unitIcon;
     public string unitName;
+    public UnitFactionEnum unitFaction;
     public UnitTypeEnum unitType;
     [Range(1,3)] public int unitLevel;
     public int unitCost;
