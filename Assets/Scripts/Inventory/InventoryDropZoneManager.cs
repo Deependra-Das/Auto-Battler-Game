948b534d6a3c6fe399381dda26444958440f6a4e
Deependra-Das <iamdeep75@gmail.com>
2026-01-28 23:03:47 +0530

    Updated InventoryDropZoneManager script

--- Assets/Scripts/Inventory/InventoryDropZoneManager.cs
+++ Assets/Scripts/Inventory/InventoryDropZoneManager.cs
@@ -1,32 +1,54 @@
 using AutoBattler.Main;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InventoryDropZoneManager : MonoBehaviour, IDropHandler
 {
-    private SpriteRenderer _highlightPanel;
+    [SerializeField] private Image _highlightInventoryPanel;
+    [SerializeField] private Color _validColor;
+    [SerializeField] private Color _wrongColor;
 
     private InventoryService _inventoryServiceObj;
     private TeamService _teamServiceObj;
+
+    private void Awake()
+    {
+        _highlightInventoryPanel.gameObject.SetActive(false);
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
         BaseUnit unit = eventData.pointerDrag?.GetComponent<BaseUnit>();
         if (unit == null) return;
 
-        unit.MarkDroppedOnValidZone();
-
         _inventoryServiceObj = GameManager.Instance.Get<InventoryService>();
+        Debug.Log(_inventoryServiceObj.CanAddUnit +" "+ _inventoryServiceObj.MaxInventorySize);
+        if (!_inventoryServiceObj.CanAddUnit) return;
+
+        UnitDragHandler dragHandler = eventData.pointerDrag.GetComponent<UnitDragHandler>();
+        if (dragHandler != null)
+            dragHandler.MarkDroppedOnValidZone();
+
         _inventoryServiceObj.AddUnit(unit.UnitData);
 
         _teamServiceObj = GameManager.Instance.Get<TeamService>();
         _teamServiceObj.RemoveUnitFromTeam(unit, unit.Team);
         Destroy(unit.gameObject);
-
     }
 
-    public void SetHighlight(bool active)
+    public void OnInteractSetHighlight(bool active)
     {
-        _highlightPanel.gameObject.SetActive(active);
+        if (!active)
+        {
+            _highlightInventoryPanel.gameObject.SetActive(false);
+            return;
+        }
+
+        _inventoryServiceObj = GameManager.Instance.Get<InventoryService>();
+        bool canAdd = _inventoryServiceObj.CanAddUnit;
+
+        _highlightInventoryPanel.gameObject.SetActive(true);
+        _highlightInventoryPanel.color = canAdd ? _validColor : _wrongColor;
     }
 }
