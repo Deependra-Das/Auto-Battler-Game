18f0a12ea8d283909915b2c6c8515097c1aa7e77
Deependra-Das <iamdeep75@gmail.com>
2026-02-06 23:44:12 +0530

    Updated DiscardUnitDropZoneManager

--- Assets/Scripts/UI/GameplayUI/DiscardUnitDropZoneManager.cs
+++ Assets/Scripts/UI/GameplayUI/DiscardUnitDropZoneManager.cs
@@ -5,20 +5,43 @@ using UnityEngine.UI;
 
 public class DiscardUnitDropZoneManager : MonoBehaviour, IDropHandler
 {
-    [SerializeField] private Image _highlightInventoryPanel;
-    [SerializeField] private Color _validColor;
-    [SerializeField] private Color _wrongColor;
+    [SerializeField] private Image _highlightDiscardUnitPanel;
+    [SerializeField] private Color _highlightColor;
 
     private void Awake()
     {
-        _highlightInventoryPanel.gameObject.SetActive(false);
+        _highlightDiscardUnitPanel.gameObject.SetActive(false);
     }
 
     public void OnDrop(PointerEventData eventData)
-    {        
+    {
+        var gameObject = eventData.pointerDrag.gameObject;
+        if (!gameObject) return;
+
+        int refundAmount = 0;
+        if (gameObject.TryGetComponent<BaseUnit>(out BaseUnit baseUnit))
+        {
+            refundAmount = baseUnit.UnitData.unitLevel;
+            Destroy(baseUnit.gameObject);
+        }
+        else if (gameObject.TryGetComponent<InventoryUnitCard>(out InventoryUnitCard inventoryUnitCard))
+        {
+            refundAmount = inventoryUnitCard.UnitData.unitLevel;
+            Destroy(inventoryUnitCard.gameObject);
+        }
+
+        GameManager.Instance.Get<CurrencyService>().AddCurrency(refundAmount);
     }
 
     public void OnInteractSetHighlight(bool active)
     {
+        if (!active)
+        {
+            _highlightDiscardUnitPanel.gameObject.SetActive(false);
+            return;
+        }
+
+        _highlightDiscardUnitPanel.gameObject.SetActive(true);
+        _highlightDiscardUnitPanel.color = _highlightColor;
     }
 }
