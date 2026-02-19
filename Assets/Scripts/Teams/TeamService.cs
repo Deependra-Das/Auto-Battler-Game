787a474022848ed0d0d0b770c62fff520f95bde2
Deependra-Das <iamdeep75@gmail.com>
2026-02-19 22:35:19 +0530

    Updated TeamService

--- Assets/Scripts/Teams/TeamService.cs
+++ Assets/Scripts/Teams/TeamService.cs
@@ -1,43 +1,89 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TeamService
 {
-    private Dictionary<TeamEnum, List<BaseUnit>> _teams = new Dictionary<TeamEnum, List<BaseUnit>>();
+    private readonly Dictionary<TeamEnum, List<BaseUnit>> _teamUnits = new();
+    private readonly Dictionary<TeamEnum, List<BaseUnit>> _inventoryUnits = new();
+    private readonly Dictionary<TeamEnum, List<BaseUnit>> _fieldUnits = new();
 
     private Dictionary<TeamEnum, int> _teamCapacities;
+    private readonly Dictionary<TeamEnum, int> _fieldCapacities;
 
-    public int GetTeamCapacity(TeamEnum team) => _teamCapacities[team];
-
-    public TeamService()
+    public TeamService(int defaultTeamCapacity = 8, int defaultFieldCapacity = 4)
     {
-        _teams.Add(TeamEnum.Team1, new List<BaseUnit>());
-        _teams.Add(TeamEnum.Team2, new List<BaseUnit>());
-        _teamCapacities = new Dictionary<TeamEnum, int> {{ TeamEnum.Team1, 4 },{ TeamEnum.Team2, 4 }};
+        _teamCapacities = new Dictionary<TeamEnum, int>();
+        _fieldCapacities = new Dictionary<TeamEnum, int>();
+
+        foreach (TeamEnum team in Enum.GetValues(typeof(TeamEnum)))
+        {
+            _teamUnits[team] = new List<BaseUnit>();
+            _inventoryUnits[team] = new List<BaseUnit>();
+            _fieldUnits[team] = new List<BaseUnit>();
+
+            _teamCapacities[team] = defaultTeamCapacity;
+            _fieldCapacities[team] = defaultFieldCapacity;
+        }
     }
 
     public bool AddUnitToTeam(BaseUnit unit, TeamEnum team)
     {
-        var teamList = _teams[team];
-
-        if (teamList.Count >= _teamCapacities[team])
-            return false;
+        var teamList = _teamUnits[team];
+        if (teamList.Contains(unit)) return false;
+        if (teamList.Count >= _teamCapacities[team]) return false;
 
         teamList.Add(unit);
+
+        _inventoryUnits[team].Add(unit);
         return true;
     }
 
     public bool RemoveUnitFromTeam(BaseUnit unit, TeamEnum team)
     {
-        return _teams[team].Remove(unit);
+        _fieldUnits[team].Remove(unit);
+        _inventoryUnits[team].Remove(unit);
+        return _teamUnits[team].Remove(unit);
     }
 
-    public List<BaseUnit> GetTeamUnits(TeamEnum team)
+    public IReadOnlyList<BaseUnit> GetTeamUnits(TeamEnum team) => _teamUnits[team].AsReadOnly();
+
+    public IReadOnlyList<BaseUnit> GetInventoryUnits(TeamEnum team) => _inventoryUnits[team].AsReadOnly();
+
+    public IReadOnlyList<BaseUnit> GetFieldUnits(TeamEnum team) => _fieldUnits[team].AsReadOnly();
+
+    public bool MoveToField(BaseUnit unit, TeamEnum team)
     {
-        if (_teams.ContainsKey(team))
-        {
-            return _teams[team];
-        }
-        return null;
+        if (!_inventoryUnits[team].Contains(unit)) return false;
+        if (_fieldUnits[team].Count >= _fieldCapacities[team]) return false;
+
+        _inventoryUnits[team].Remove(unit);
+        _fieldUnits[team].Add(unit);
+        return true;
     }
+
+    public bool MoveToInventory(BaseUnit unit, TeamEnum team)
+    {
+        if (!_fieldUnits[team].Contains(unit)) return false;
+
+        _fieldUnits[team].Remove(unit);
+        _inventoryUnits[team].Add(unit);
+        return true;
+    }
+
+    public void ResetFieldUnits(TeamEnum team)
+    {
+        var units = _fieldUnits[team].ToList();
+        foreach (var unit in units)
+        {
+            MoveToInventory(unit, team);
+        }
+    }
+
+    public int GetTeamCapacity(TeamEnum team) => _teamCapacities[team];
+    public int GetFieldCapacity(TeamEnum team) => _fieldCapacities[team];
+
+    public void SetTeamCapacity(TeamEnum team, int capacity) => _teamCapacities[team] = capacity;
+    public void SetFieldCapacity(TeamEnum team, int capacity) => _fieldCapacities[team] = capacity;
 }
