bc2404648c422cc1ebe3538f5413e98a1ce3325e
Deependra-Das <iamdeep75@gmail.com>
2026-03-09 20:03:17 +0530

    Added BuffNameEnum

---
+++ Assets/Scripts/Buffs/BuffNameEnum.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public enum BuffNameEnum
+{
+    AttackDamageBuff,
+    ShieldBuff,
+    AttackSpeedBuff,
+    HealthHealBuff,
+    FireDamageBuff,
+    ThunderDamageBuff,
+    NatureDamageBuff,
+}
