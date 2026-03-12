7d481fc85267a81c23b5990dbcab797591f49ad2
Deependra-Das <iamdeep75@gmail.com>
2026-03-12 23:42:38 +0530

    Changes related to events to manage buffs based on onfield unit type & faction count

--- Assets/Scripts/Buffs/BuffService.cs
+++ Assets/Scripts/Buffs/BuffService.cs
@@ -1,25 +1,55 @@
+using AutoBattler.Event;
+using AutoBattler.Main;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BuffService
 {
     private Dictionary<BuffNameEnum, BuffData> _buffData = new();
-    private Dictionary<BuffNameEnum, int> _appliedBuffs = new();
+    private Dictionary<TeamEnum, Dictionary<BuffNameEnum, int>> _appliedBuffs = new();
 
     public BuffService(BuffScriptableObjectScript buff_SO)
     {
-        foreach(var buffs in buff_SO.buffData)
+        SubscribeToEvents();
+
+        foreach (var buffs in buff_SO.buffData)
         {
             _buffData.Add(buffs.buffName, buffs);
         }
     }
 
+    ~BuffService() 
+    {
+        UnsubscribeToEvents();
+    }
+
+    void SubscribeToEvents()
+    {
+        EventBusManager.Instance.Subscribe(EventNameEnum.UnitAddedOnField, OnUnitAddedOnField_Buff);
+    }
+
+    void UnsubscribeToEvents()
+    {
+        EventBusManager.Instance.Unsubscribe(EventNameEnum.UnitAddedOnField, OnUnitAddedOnField_Buff);
+    }
+
+
     public void InitializeBuffs()
     {
-        foreach(var buffs in _buffData)
+        foreach (TeamEnum team in Enum.GetValues(typeof(TeamEnum)))
         {
-            UIManager.Instance.AddBuffDetailUICard(buffs.Value);
-            _appliedBuffs.Add(buffs.Key, 0);
+            _appliedBuffs[team] = new Dictionary<BuffNameEnum, int>();
+
+            foreach (var buffs in _buffData)
+            {
+                _appliedBuffs[team].Add(buffs.Key, 0);
+
+                if (team == TeamEnum.Team1)
+                {
+                    UIManager.Instance.AddBuffDetailUICard(buffs.Value);
+                }
+            }
         }
     }
 
@@ -28,8 +58,42 @@ public class BuffService
         _appliedBuffs.Clear();
     }
 
-    private void UpdateAppliedBuffs()
+    private void OnUnitAddedOnField_Buff(object[] parameters)
     {
+        TeamEnum team = (TeamEnum)parameters[0];
+        UnitTypeEnum type = (UnitTypeEnum)parameters[1];
+        UnitFactionEnum faction = (UnitFactionEnum)parameters[2];
 
+        UpdateAppliedBuffsParticipants(team, type, faction);
+    }
+
+    private void UpdateAppliedBuffsParticipants(TeamEnum team, UnitTypeEnum type, UnitFactionEnum faction)
+    {
+        int typeParticpantCount = GameManager.Instance.Get<TeamService>().GetTypeCount(team, type);
+        int factionParticpantCount = GameManager.Instance.Get<TeamService>().GetFactionCount(team, faction);
+
+        switch (type)
+        {
+            case UnitTypeEnum.Attacker:
+                break;
+            case UnitTypeEnum.Ranged:
+                break;
+            case UnitTypeEnum.Support:
+                break;
+            case UnitTypeEnum.Tank:
+                break;
+        }
+
+        switch (faction)
+        {
+            case UnitFactionEnum.Crusader:
+                break;
+            case UnitFactionEnum.Spartan:
+                break;
+            case UnitFactionEnum.Viking:
+                break;
+        }
+
+        Debug.Log(typeParticpantCount + "--" + factionParticpantCount);
     }
 }
