0f235e67cfa976deb0897e55a81f3ecd07799c5b
Deependra-Das <iamdeep75@gmail.com>
2026-03-25 23:14:54 +0530

    Added TeamBuffData struct

---
+++ Assets/Scripts/Buffs/TeamBuffData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public struct TeamBuffData
+{
+    public float attackBonus;
+    public float shieldBonus;
+    public float hpBonus;
+    public float attackSpeedBonus;
+    public float fireDamageBonus;
+    public float thunderDamageBonus;
+    public float natureDamageBonus;
+}
