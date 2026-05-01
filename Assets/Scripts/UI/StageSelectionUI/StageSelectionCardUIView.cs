c45aed9a2ed9a6a80d5a50848fe25178636ab14a
Deependra-Das <iamdeep75@gmail.com>
2026-05-01 16:09:33 +0530

    Added StageSelectionCardUIView script

---
+++ Assets/Scripts/UI/StageSelectionUI/StageSelectionCardUIView.cs
@@ -0,0 +1,38 @@
+using AutoBattler.Main;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelectionCardUIView : MonoBehaviour
+{
+    private int _stageIndex;
+    [SerializeField] private TMP_Text _stageNameText;
+    [SerializeField] private TMP_Text _numberOfRoundsText;
+    [SerializeField] private Button _stageButton;
+
+    private void OnEnable() => SubscribeToEvents();
+    private void OnDisable() => UnsubscribeToEvents();
+
+    void SubscribeToEvents()
+    {
+        _stageButton.onClick.AddListener(OnStageButtonClicked);
+    }
+
+    void UnsubscribeToEvents()
+    {
+        _stageButton.onClick.RemoveListener(OnStageButtonClicked);
+    }
+
+    public void Initialize(int stageIndex, StageData stageData)
+    {
+        _stageIndex = stageIndex;
+        _numberOfRoundsText.text = stageData.roundDataList.Count.ToString();
+    }
+
+    private void OnStageButtonClicked()
+    {
+        Debug.Log(_stageIndex);
+        GameManager.Instance.currentStageIndexSelected = _stageIndex;
+        SceneLoader.Instance.LoadScene(SceneNameEnum.GameplayScene);
+    }
+}
