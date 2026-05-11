df249c8b8259908d697fddd3c520e7599699b6c9
Deependra-Das <iamdeep75@gmail.com>
2026-05-11 17:13:22 +0530

    Added StageSnapshotData script

---
+++ Assets/Scripts/Stage/StageSnapshotData.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class StageSnapshotData
+{
+    public List<RoundSnapshotData> stageSnapshotList = new();
+}
