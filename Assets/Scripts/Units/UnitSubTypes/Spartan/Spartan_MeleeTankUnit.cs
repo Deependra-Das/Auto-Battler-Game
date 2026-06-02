e9a2b48b0a79cc82a84486faca8c9f21278e0eea
Deependra-Das <iamdeep75@gmail.com>
2026-06-02 15:55:19 +0530

    Updated Spartan Melee Tank to perform attacks using events

--- Assets/Scripts/Units/UnitSubTypes/Spartan/Spartan_MeleeTankUnit.cs
+++ Assets/Scripts/Units/UnitSubTypes/Spartan/Spartan_MeleeTankUnit.cs
@@ -18,7 +18,11 @@ public class Spartan_MeleeTankUnit : BaseUnit
     private void PerformWarAxeAttack()
     {
         animator.SetTrigger("Attack");
-        currentTarget.TakeDamage(unitData.baseDamage, unitData.unitElement);
         StartCoroutine(AttackCoolDownWaitCoroutine());
+    }
+
+    private void SpartanPolearmThrust()
+    {
+        currentTarget.TakeDamage(unitData.baseDamage, unitData.unitElement);
     }
 }
