461f209b93311bfb2ab28c25c7a90f37cba2dbdf
Deependra-Das <iamdeep75@gmail.com>
2026-06-04 17:32:49 +0530

    Updated GameManager to add references to UnitDataService & UnitPoolService

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -7,7 +7,8 @@ namespace AutoBattler.Main
     public class GameManager : GenericMonoSingleton<GameManager>
     {
         [SerializeField] private TileScriptableObjectScript _tile_SO;
-        [SerializeField] private UnitDataScriptableObjectScript _unit_SO;
+        [SerializeField] private UnitDataScriptableObjectScript _unitData_SO;
+        [SerializeField] private UnitPrefabScriptableObjectScript _unitPrefab_SO;
         [SerializeField] private RangedAbilitiesScriptableObjectScript _rangedAbilities_SO;
         [SerializeField] private BuffScriptableObjectScript _buff_SO;
         [SerializeField] private StageConfigScriptableObjectScript _stageConfig_SO;
@@ -33,13 +34,14 @@ namespace AutoBattler.Main
 
         private void RegisterServices()
         {
+            ServiceLocator.Register(new UnitDataService(_unitData_SO));
+            ServiceLocator.Register(new UnitPoolService(_unitPrefab_SO));
             ServiceLocator.Register(new TileGridService(_tile_SO));
             ServiceLocator.Register(new GraphService());
             ServiceLocator.Register(new TeamService());
             ServiceLocator.Register(new RangedAbilityService(_rangedAbilities_SO));
             ServiceLocator.Register(new CurrencyService());
             ServiceLocator.Register(new PlayerLevelService(_playerLevelConfig_SO));
-            ServiceLocator.Register(new UnitDataService(_unit_SO));
             ServiceLocator.Register(new InventoryService());
             ServiceLocator.Register(new ShopService());
             ServiceLocator.Register(new BuffService(_buff_SO));
@@ -50,6 +52,8 @@ namespace AutoBattler.Main
 
         private void DeregisterServices()
         {
+            ServiceLocator.Unregister<UnitDataService>();
+            ServiceLocator.Unregister<UnitPoolService>();
             ServiceLocator.Unregister<TileGridService>();
             ServiceLocator.Unregister<GraphService>();
             ServiceLocator.Unregister<TeamService>();
@@ -57,7 +61,6 @@ namespace AutoBattler.Main
             ServiceLocator.Unregister<StageService>();
             ServiceLocator.Unregister<CurrencyService>();
             ServiceLocator.Unregister<PlayerLevelService>();
-            ServiceLocator.Unregister<UnitDataService>();
             ServiceLocator.Unregister<InventoryService>();
             ServiceLocator.Unregister<ShopService>();
             ServiceLocator.Unregister<BuffService>();
