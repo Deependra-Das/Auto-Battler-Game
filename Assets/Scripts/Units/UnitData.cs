5dc5f5502ef76528b1c4d18b02bc622d782b2d91
Deependra-Das <iamdeep75@gmail.com>
2026-06-08 10:53:14 +0530

    Updated UnitData to include attackAnimationDelay

--- Assets/Scripts/Units/UnitData.cs
+++ Assets/Scripts/Units/UnitData.cs
@@ -9,18 +9,18 @@ public struct UnitData
     public string unitName;
     public UnitFactionEnum unitFaction;
     public UnitTypeEnum unitType;
-    [Range(1,3)] public int unitLevel;
     public UnitElementEnum unitElement;
-    public int unitCost;
+    [Range(1,3)] public int unitLevel;
+    [Range(1, 5)] public int baseRange;
 
+    public int baseUnitCost;
     public int baseDamage;
     public int baseHealth;
     public int baseShield;
     public int baseHealing;
     public float baseAttackSpeed;
     public float baseMovementSpeed;
-    [Range(1, 5)] public int baseRange;
-    public float delayBeforeRangedAttack;
+    public float attackAnimationDelay;
     public float baseElementalDamageScalingFactor;
     public float teamShieldScalingFactor;
 }
