38fd21e8023425aa7d45394d1e0bc0dcef06cb76
Deependra-Das <iamdeep75@gmail.com>
2026-06-12 13:29:52 +0530

    Updated Tile_SO & Added HighlightTilePrefab

--- Assets/Scripts/Tile/ScriptableObjects/TileScriptableObjectScript.cs
+++ Assets/Scripts/Tile/ScriptableObjects/TileScriptableObjectScript.cs
@@ -3,9 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TileScriptableObjectScript", menuName = "ScriptableObjects/TileScriptableObjectScript")]
 public class TileScriptableObjectScript : ScriptableObject
 {
-    public Tile tilePrefab;
-    public int mapWidth;
-    public int mapHeight;
-    public float tileOffset_X;
-    public float tileOffset_Y;
+    public GameObject highlightTilePrefab;
+    public Sprite validHighlightSprite;
+    public Sprite invalidHighlightSprite;
 }
