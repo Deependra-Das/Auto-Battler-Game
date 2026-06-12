e3fbeb52ba04f9377249f0066f5c3666f4d2a038
Deependra-Das <iamdeep75@gmail.com>
2026-06-12 13:29:00 +0530

    Updated TileGridService to use Tilemap

--- Assets/Scripts/Tile/TileGridService.cs
+++ Assets/Scripts/Tile/TileGridService.cs
@@ -1,65 +1,27 @@
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TileGridService
 {
-    private Tile _tilePrefab;
-    private int _mapWidth;
-    private int _mapHeight;
-    private float _tileOffset_X;
-    private float _tileOffset_Y;
+    private Tilemap _spawnedTileMap;
+    private Grid _tileGrid;
 
-    private List<Tile> _spawnedTileList = new List<Tile>();
-
-    public TileGridService(TileScriptableObjectScript tile_SO)
+    public void CreateTileMap(Tilemap _tileMapPrefab, Grid tileGrid)
     {
-        _tilePrefab = tile_SO.tilePrefab;
-        _mapWidth = tile_SO.mapWidth;
-        _mapHeight = tile_SO.mapHeight;
-        _tileOffset_X = tile_SO.tileOffset_X;
-        _tileOffset_Y = tile_SO.tileOffset_Y;
-    }
-
-    public void CreateTileMap()
-    {
-        float totalWidth = _mapWidth * _tileOffset_X;
-        float totalHeight = _mapHeight * _tileOffset_Y;
-
-        Vector3 centerPosition = new Vector3(-totalWidth / 2 + _tileOffset_X / 2, -totalHeight / 2, 0);
-
-        for (int y = 0; y < _mapHeight; y++)
-        {
-            int tilesInRow = (y % 2 == 0) ? _mapWidth : _mapWidth - 1;
-
-            for (int x = 0; x < tilesInRow; x++)
-            {
-                Tile tile = GameObject.Instantiate(_tilePrefab);
-
-                float offsetX = (y % 2 == 0) ? x * _tileOffset_X : x * _tileOffset_X + _tileOffset_X / 2;
-                tile.transform.position = centerPosition + new Vector3(offsetX, y * _tileOffset_Y, 0);
-
-                _spawnedTileList.Add(tile);
-            }
-        }
+        _tileGrid = tileGrid;
+        _spawnedTileMap = GameObject.Instantiate(_tileMapPrefab, _tileGrid.transform);
     }
 
     public void Reset()
     {
-        foreach (Tile tile in _spawnedTileList)
+        if (_spawnedTileMap != null)
         {
-            if (tile != null)
-            {
-                GameObject.Destroy(tile.gameObject);
-            }
+            Object.Destroy(_spawnedTileMap.gameObject);
+            _spawnedTileMap = null;
         }
 
-        _spawnedTileList.Clear();
+        _tileGrid = null;
     }
 
-    public List<Tile> GetSpawnedTilesList()
-    {
-        if(_spawnedTileList != null) return _spawnedTileList;
-
-        return null;
-    }
+    public Tilemap CurrentTileMap => _spawnedTileMap;
 }
