a4544a91a6e4b2dfce488e9a4ef9d743c0812e1f
Deependra-Das <iamdeep75@gmail.com>
2026-06-20 17:55:21 +0530

    Added RangedAbilityElementTrailMaterialEntry script

---
+++ Assets/Scripts/RangedAbilities/RangedAbilityElementTrailMaterialEntry.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct RangedAbilityElementTrailMaterialEntry
+{
+    public UnitElementEnum element;
+    public Material outerTrailMaterial;
+    public Material innerTrailMaterial;
+}
